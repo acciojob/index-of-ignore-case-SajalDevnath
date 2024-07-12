659497260bd26000c39fb22ee12f77548550a1d4
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-12 19:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+    let lowerStr = str.toLowerCase();
+    let lowerSubStr = subStr.toLowerCase();
+    
+    let index = lowerStr.indexOf(lowerSubStr);
+    
+    
+    return index;
 }
 
+// Example usage:
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
